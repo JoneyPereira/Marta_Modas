73994af98117b5d7333dadbede41c20200decf2b
JoneyPereira <38810506+JoneyPereira@users.noreply.github.com>
2018-12-17 19:40:03 -0300

    Update config.php
    Teste de hospedagem.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CodeIgniter/Marta_Modas';
+$config['base_url'] = 'https://martamodas.com.br';
 
 /*
 |--------------------------------------------------------------------------
