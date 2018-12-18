f7af0f08e4efde0a63016465ae18855315324a4f
JoneyPereira <38810506+JoneyPereira@users.noreply.github.com>
2018-12-18 19:33:10 -0300

    Update config.php
    Removendo o caminho principal.

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://martamodas.com.br';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
